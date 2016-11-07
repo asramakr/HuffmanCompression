3c36327dc7f9d186ac9ee8b6b681fe174221bed9
Arun S Ramakrishnan <asramakr@ieng6-201.ucsd.edu>
2016-11-06 21:26:50 -0800

    Finished Project

--- compress.cpp
+++ compress.cpp
@@ -12,6 +12,7 @@
 #include<string>
 #include<set>
 #include<cstdlib>
+#include <math.h>
 using namespace std;
 
 
@@ -21,6 +22,7 @@ int main(int argc, char** argv)
   unsigned char nextChar; //the next char to be read
   int nextByte; //the next byte, which checks EOF
   vector<int> freqs; //the vector of frequencies of each letter in the tree
+  vector<int> freqsInit;
   HCTree newTree; //the HCTree
   //unsigned int numOfSymbols = 0;
 
@@ -31,6 +33,7 @@ int main(int argc, char** argv)
 
   for(int i=0; i<256; i++){
     freqs.push_back(0); //pushes each letter into the vector
+    freqsInit.push_back(0);
   }
 
   while ((nextByte = infile.get()) != EOF) {
@@ -48,20 +51,87 @@ int main(int argc, char** argv)
   outfile.open(argv[2], std::ios::binary); //opens it so it can write to it
   BitOutputStream out(outfile);
 
+  /*
   //writes the header to the outfile, aka the freqs of each character
   for(int i=0; i<256; i++){
     outfile << freqs[i];
     outfile << '\n';
   }
+  */
 
-  infile.open(argv[1], std::ios::binary); //opens the file again
+  //new header tests
+
+  int header1 = 0;
+  int header2 = 0;
+  int header3 = 0;
+  int header4 = 0;
+  int header5 = 0;
+  int header6 = 0;
+  int header7 = 0;
+  int header8 = 0;
+
+  for(int i=0; i<256; i++){
+    if(freqs[i] != 0){
+      cout << "Chars In Message: " << i << endl;
+      cout <<"Number of Chars: " << freqs[i] << endl;
+      freqsInit[i] = 1;
+    }
+  }
+
+  for(int i=0; i<32; i++){
+    header1 = header1 | (int)(freqsInit[i]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header2 = header2 | (int)(freqsInit[i+32]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header3 = header3 | (int)(freqsInit[i+64]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header4 = header4 | (int)(freqsInit[i+96]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header5 = header5 | (int)(freqsInit[i+128]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header6 = header6 | (int)(freqsInit[i+160]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header7 = header7 | (int)(freqsInit[i+192]*pow(2, 31-i));
+  }
+  for(int i=0; i<32; i++){
+    header8 = header8 | (int)(freqsInit[i+224]*pow(2, 31-i));
+  }
+
+  outfile << header1 << endl;
+  outfile << header2 << endl;
+  outfile << header3 << endl;
+  outfile << header4 << endl;
+  outfile << header5 << endl;
+  outfile << header6 << endl;
+  outfile << header7 << endl;
+  outfile << header8 << endl;
+
+
+
+  for(int i=0; i<256; i++){
+    if(freqs[i] != 0){
+      outfile << freqs[i] << endl;
+    }
+  }
+
+  
+
+  //opens file again
+  infile.open(argv[1], std::ios::binary);
  
   //pulls the bytes from the infile
   while((nextByte = infile.get()) != EOF){
-    nextChar = (unsigned char)nextByte; 
     if (infile.eof()) {
       break;
-    }
+    }    
+    nextChar = (unsigned char)nextByte; 
+    cout << "NextByte: " << nextChar << endl;
     //cout << "ENCODING " << nextChar << endl;
     newTree.encode( nextChar, out); //encodes the byte
 //    cout << "nextChar: " << nextChar << endl;
--- uncompress.cpp
+++ uncompress.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <set>
 #include <cstdlib>
+#include <math.h>
 #define HEADERSIZE 256
 using namespace std;
 
@@ -25,6 +26,9 @@ int main(int argc, char** argv)
   vector<int> freqs; // vector of freqs of each char from infile
   HCTree newTree; // tree to build using freqs
   string readLine; // var to hold each read in freq
+  vector<int> totalFreq;
+  int counter = 0;
+  int totalSize = 0;
 
   // initialize freqs to 0
   for (int i = 0; i < HEADERSIZE; i++) {
@@ -35,12 +39,39 @@ int main(int argc, char** argv)
   infile.open(argv[1], std::ios::binary);
   BitInputStream in(infile);
 
+  /* old header reading system
   // read 256 lines of freqs
   for (int i = 0; i < HEADERSIZE; i++) {
     getline(infile, readLine);
     freqCount = stoi(readLine); // convert read string to int
     freqs.at(i) = freqCount; // insert freq into index
   }
+  */
+
+  //new header reading system
+  
+  for(int i = 0; i<8; i++){
+    getline(infile, readLine);
+    freqCount = stoi(readLine);
+    for(int j = 0; j<32; j++){
+      freqs.at((i*32)+j) = freqCount & (int)(pow(2, 31-j));
+      if(freqs.at((i*32)+j) != 0){
+        totalFreq.push_back((i*32)+j);
+      }
+    }
+  }
+  for(int i=0; i<totalFreq.size(); i++){
+    getline(infile, readLine);
+    freqCount = stoi(readLine);
+    freqs.at(totalFreq.at(i)) = freqCount;
+    cout << "Chars In Message: " << totalFreq.at(i) << endl;
+    cout << "Number of Chars: " << freqs[totalFreq.at(i)] << endl;    
+  }
+
+  for(int i=0; i<freqs.size(); i++){
+    totalSize+=freqs[i];
+  }
+  
 
   // build tree from frequencies
   newTree.build(freqs);
@@ -50,7 +81,7 @@ int main(int argc, char** argv)
   outfile.open(argv[2], std::ios::binary);
 
   // go through and decode the file message
-  while(1) {
+  while(counter < totalSize) {
 
     // exit if reached end of file
     if(infile.eof()) {
@@ -71,8 +102,10 @@ int main(int argc, char** argv)
 //    cout << "return: " << decodedChar << endl;
 
     // write decoded symbol to file
+    cout << "Decoded Char: " << decodedChar << endl;
     outfile << decodedChar;
-    //cout << "ENCODING: " << decodedChar << endl;    
+    //cout << "ENCODING: " << decodedChar << endl; 
+    counter++;   
   }
 
   // close files
